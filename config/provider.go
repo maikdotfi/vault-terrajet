c554d27030c62ccef62bf2e8e2b3dd80243e7ad6
maikdotfi <vainio.mike@gmail.com>
2022-08-02 15:10:34 +0300

    Commit after prepare.sh

--- config/provider.go
+++ config/provider.go
@@ -23,12 +23,12 @@ import (
 	tjconfig "github.com/crossplane/terrajet/pkg/config"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
-	"github.com/crossplane-contrib/provider-jet-template/config/null"
+	"github.com/crossplane-contrib/provider-jet-vault/config/null"
 )
 
 const (
-	resourcePrefix = "template"
-	modulePath     = "github.com/crossplane-contrib/provider-jet-template"
+	resourcePrefix = "vault"
+	modulePath     = "github.com/crossplane-contrib/provider-jet-vault"
 )
 
 //go:embed schema.json
